3fecc3e856d4fb81226b1ed9f36f9ee35db11ee0
jinn_macp <vz.jinn@gmail.com>
2024-03-01 23:10:55 +0900

    added t2.py t3.py

---
+++ 10-language/10-python/t3.py
@@ -0,0 +1,20 @@
+import enum
+
+
+@enum.unique
+class Status(enum.Enum):
+    SUCCESS = 1
+    OK = 2
+    FAIL = 3
+    WRONG = 4
+
+def main():
+    for s in Status:
+        print(s.name)
+
+    print(Status.__members__)
+
+    print(Status.SUCCESS == Status.OK)
+
+if __name__ == "__main__":
+    main()
